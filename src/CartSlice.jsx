46a93d9b29d4d545ab9bf49338bc12b74ff504b8
nanasnumber <nanasnumber@gmail.com>
2024-07-27 15:31:50 -0400

    Global state integration with Redux (addItem, updateItem, removeItem)

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,34 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
+      const { type, name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        switch (type) {
+          case ('inc'):
+            itemToUpdate.quantity++;
+            break;
+          case ('dec'):
+            if (quantity <= 0) {
+              break;
+            }
 
-    
+            itemToUpdate.quantity--;
+            break;
+        }
+      }
     },
   },
 });
