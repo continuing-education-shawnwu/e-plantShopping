4dd2e213906cc583cec7d237b12ea32263957e47
nanasnumber <nanasnumber@gmail.com>
2024-07-28 14:36:00 -0400

    Add item price to card

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -317,6 +317,7 @@ function ProductList() {
                   <div className="product-card" key={plantIndex}>
                     <img className="product-image" src={plant.image} alt={plant.name} />
                     <div className="product-title">{plant.name}</div>
+                    <div className="product-price">{plant.cost}</div>
                     {!itemInCart(plant.name) ? (
                       <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                     ) : (
